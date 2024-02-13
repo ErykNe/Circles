1abe4b72ce84c59bc5fa439ccd17a1bf8bc96b27
Eryk Nejfert <110306022+ErykNe@users.noreply.github.com>
2024-02-13 22:43:52 +0100

    App made

---
+++ Circles/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Circles
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            GenerateCirclesButton = new Button();
+            label1 = new Label();
+            amountTextBox = new TextBox();
+            outsideButton = new RadioButton();
+            insideButton = new RadioButton();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ControlLightLight;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(46, 37);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(740, 740);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // GenerateCirclesButton
+            // 
+            GenerateCirclesButton.BackColor = SystemColors.Highlight;
+            GenerateCirclesButton.Font = new Font("Impact", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            GenerateCirclesButton.Location = new Point(877, 437);
+            GenerateCirclesButton.Name = "GenerateCirclesButton";
+            GenerateCirclesButton.Size = new Size(433, 121);
+            GenerateCirclesButton.TabIndex = 1;
+            GenerateCirclesButton.Text = "GENERATE";
+            GenerateCirclesButton.UseVisualStyleBackColor = false;
+            GenerateCirclesButton.Click += GenerateCirclesButton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label1.ForeColor = SystemColors.HotTrack;
+            label1.Location = new Point(877, 151);
+            label1.Name = "label1";
+            label1.Size = new Size(150, 21);
+            label1.TabIndex = 2;
+            label1.Text = "Amount of Circles:";
+            // 
+            // amountTextBox
+            // 
+            amountTextBox.ForeColor = SystemColors.MenuText;
+            amountTextBox.Location = new Point(877, 187);
+            amountTextBox.Name = "amountTextBox";
+            amountTextBox.Size = new Size(433, 23);
+            amountTextBox.TabIndex = 3;
+            // 
+            // outsideButton
+            // 
+            outsideButton.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            outsideButton.Location = new Point(904, 320);
+            outsideButton.Name = "outsideButton";
+            outsideButton.Size = new Size(283, 30);
+            outsideButton.TabIndex = 4;
+            outsideButton.TabStop = true;
+            outsideButton.Text = "Can only be outside the other";
+            outsideButton.UseVisualStyleBackColor = true;
+            // 
+            // insideButton
+            // 
+            insideButton.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold);
+            insideButton.Location = new Point(904, 356);
+            insideButton.Name = "insideButton";
+            insideButton.Size = new Size(242, 30);
+            insideButton.TabIndex = 8;
+            insideButton.TabStop = true;
+            insideButton.Text = "Can also be inside the other";
+            insideButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label2.Location = new Point(877, 275);
+            label2.Name = "label2";
+            label2.Size = new Size(220, 21);
+            label2.TabIndex = 9;
+            label2.Text = "Select one of the following:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1361, 807);
+            Controls.Add(label2);
+            Controls.Add(insideButton);
+            Controls.Add(outsideButton);
+            Controls.Add(amountTextBox);
+            Controls.Add(label1);
+            Controls.Add(GenerateCirclesButton);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button GenerateCirclesButton;
+        private Label label1;
+        private TextBox amountTextBox;
+        private RadioButton outsideButton;
+        private RadioButton insideButton;
+        private Label label2;
+    }
+}
---
+++ Circles/Form1.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Circles
+{
+    public partial class Form1 : Form
+    {
+        private Random randomColor = new Random();
+        private Random randomVariable = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void GenerateCirclesButton_Click(object sender, EventArgs e)
+        {
+            List<Circle> circles = new List<Circle>();
+            Graphics g = pictureBox1.CreateGraphics();
+
+            g.Clear(Color.White);
+            int amountOfCircles = Convert.ToInt32(amountTextBox.Text.Trim());
+            label1.Text = amountOfCircles.ToString();
+
+            for (int i = 0; i < amountOfCircles; i++)
+            {
+                Brush b = new SolidBrush(Color.FromArgb(randomColor.Next(0, 255), randomColor.Next(0, 255), randomColor.Next(0, 255)));
+
+                Circle circle = new Circle();
+                circle.x = randomVariable.Next(10, 730);
+                circle.y = randomVariable.Next(10, 730);
+                circle.radius = Convert.ToInt32(randomVariable.Next(4, 60));
+                bool isCircleLegal = true;
+
+                foreach (Circle existingCircle in circles)
+                {
+                    if (outsideButton.Checked && !outside(circle, existingCircle))
+                    {
+                        isCircleLegal = false;
+                        break; // Exit the loop if the circle is not legal
+                    }
+                    if (insideButton.Checked && !inside(circle, existingCircle) && !outside(circle, existingCircle))
+                    {
+                        isCircleLegal = false;
+                        break; // Exit the loop if the circle is not legal
+                    }
+                }
+
+                if (isCircleLegal)
+                {
+                    circles.Add(circle);
+                    g.FillEllipse(b, circle.x - circle.radius, circle.y - circle.radius, 2 * circle.radius, 2 * circle.radius);
+                }
+                else
+                {
+                    i--;
+                }
+            }
+        }
+
+        private bool outside(Circle circle1, Circle circle2)
+        {
+            double distance = Math.Sqrt(Math.Pow(circle1.x - circle2.x, 2) + Math.Pow(circle1.y - circle2.y, 2));
+            return distance > (circle1.radius + circle2.radius);
+        }
+
+        private bool inside(Circle circle1, Circle circle2)
+        {
+            double distance = Math.Sqrt(Math.Pow(circle1.x - circle2.x, 2) + Math.Pow(circle1.y - circle2.y, 2));
+            return distance + circle1.radius < circle2.radius;
+        }
+    }
+
+    public class Circle
+    {
+        public int x;
+        public int y;
+        public int radius;
+    }
+}
